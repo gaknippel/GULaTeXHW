71cd80a982a0bcbb34b62dcda1868dce4292cbea
Greyson Knippel <gaknippel@hotmail.com>
2026-02-04 15:16:34 -0800

    hello son

--- CPSC260/gobblegobble2/main.c
+++ CPSC260/gobblegobble2/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>
 
 
 int main(){
@@ -7,6 +8,19 @@ int main(){
 
     printf("Decimal: %d, Hex: %x\n", x , x);
     printf("Decimal: %d, Hex: %x\n", y , y);
+
+
+    uint8_t g = 15;
+    uint8_t h = -15;
+
+    int8_t m = 15;
+    int8_t n = -15;
+
+    printf("Decimal: %d, Hex: %x\n", g , g);
+    printf("Decimal: %d, Hex: %x\n", h , h);
+    printf("Decimal: %d, Hex: %x\n", m , m);
+    printf("Decimal: %d, Hex: %x\n", n , n);
+
 
 
     return 0;
