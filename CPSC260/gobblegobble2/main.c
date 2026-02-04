2b58f16f713bd7c5c9b831a285938cd5de036409
Greyson Knippel <gaknippel@hotmail.com>
2026-02-04 15:15:42 -0800

    added negative numbers c file

---
+++ CPSC260/gobblegobble2/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main(){
+    int x = 15;
+    int y = -15;
+
+    printf("Decimal: %d, Hex: %x\n", x , x);
+    printf("Decimal: %d, Hex: %x\n", y , y);
+
+
+    return 0;
+}
